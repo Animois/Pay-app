a65053a0582b41b6c2ac69d066aa056861e938f5
Shrayas Chakma <shrayas.6351@gmail.com>
2025-11-21 13:24:25 +0600

    added cashout

--- home.js
+++ home.js
@@ -1,3 +1,6 @@
+const validPin = 1234
+
+// Add Money Feature
 document.getElementById("add-btn").addEventListener
 ("click", function (e){
     e.preventDefault()
@@ -7,16 +10,57 @@ document.getElementById("add-btn").addEventListener
     ("account-number").value
     const ammount = parseInt(document.getElementById
     ("add-ammount").value)
-    const pin = document.getElementById
-    ("pin-number").value
+    const pin = parseInt(document.getElementById
+    ("pin-number").value)
 
     const availableBalance = parseInt(document.getElementById
     ("available-balance").innerText)
 
     console.log(availableBalance)
 
+    if(accountNumber.length !== 11){
+        alert("Account Number must be 11 digits")
+        return;
+    }
+
+    if(pin !== validPin){
+        alert("Invalid Pin")
+        return;
+    }
+
     const totalBalance = availableBalance + ammount
 
     document.getElementById("available-balance").
     innerText = totalBalance
+})
+
+// Cash Out Feature
+
+document.getElementById("withdraw-btn").addEventListener("click",
+    function(e){
+        e.preventDefault()
+        
+        const amount = parseInt(document.getElementById("withdraw-amount").
+        value)
+
+        const availableBalance = parseInt(document.getElementById
+            ("available-balance").innerText)
+
+        const totalNewAvailableBalance = availableBalance - amount
+        
+        document.getElementById("available-balance").innerText =
+        totalNewAvailableBalance
+    }
+)
+
+// toggling feature
+
+document.getElementById("add-btn").addEventListener("click", function(){
+    document.getElementById("cash-out-parent").style.display = "none"
+    document.getElementById("add-money-parent").style.display = "block"
+})
+
+document.getElementById("cash-out-btn").addEventListener("click", function(){
+    document.getElementById("add-money-parent").style.display = "none"
+    document.getElementById("cash-out-parent").style.display = "block"
 })
